51d36a3e290c89243231c909d006f18e537bc9e0
Salvador <salvador.on@outlook.com>
2020-07-30 10:31:19 -0300

    add question and category controller

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -1,18 +1,5 @@
 ActiveAdmin.register Question do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :question, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:question, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :question, :category_id
   
 end
